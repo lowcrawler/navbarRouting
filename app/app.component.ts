619cc6e7f10ea7082fe816f968c7eff61e3e0618
lowcrawler <joe.federer@gmail.com>
2016-07-20 22:52:58 -0500

    three way animations

--- app/app.component.ts
+++ app/app.component.ts
@@ -29,50 +29,62 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 			<router-outlet></router-outlet>`,
 animations: [
     	trigger('navState', [
-      		state('inactive', style({
-		        backgroundColor: '#eee',
+			state('expanded', style({
+				backgroundColor: '#ff4444',
 		        transform: 'scale(1)'
+			})),
+      		state('inactive', style({
+		        //backgroundColor: '#44ff44',
+				display: 'none',
+				transform: 'scale(0)'
 			})),
 			state('active',   style({
 				backgroundColor: '#1111ff',
 				transform: 'scale(1.5)'
 			})),
-			transition('inactive => active', animate('100ms ease-in')),
-			transition('active => inactive', animate('100ms ease-out'))
+			transition('expanded => inactive', animate('1000ms ease-out')),
+			transition('inactive => expanded', animate('1000ms ease-in')),
+			transition('active => expanded', animate('1000ms ease-out'))
+			transition('active => inactive', animate('1000ms ease-out'))
 		])
 	]
 })
 
 export class AppComponent  {
 	activeTabName = '';
-	myNavState1 = 'inactive';
-	myNavState2 = 'active';
-	myNavState3 = 'inactive';
+	myNavState1 = 'expanded';
+	myNavState2 = 'expanded';
+	myNavState3 = 'expanded';
 
 	navSelected(navName) {
 		console.log(navName + " Clicked!");
 		if(navName==this.activeTabName) {
 			this.activeTabName = '';
+			this.myNavState1='expanded';
+			this.myNavState2='expanded';
+			this.myNavState3='expanded';
 		} else {
+			this.myNavState1='inactive';
+			this.myNavState2='inactive';
+			this.myNavState3='inactive';
 			this.activeTabName = navName;
+			switch (navName) {
+			    case "mod1":
+				this.myNavState1 = 'active';
+			      break;
+			    case "mod2":
+				this.myNavState2 = 'active';
+			      break;
+			    case "mod3":
+				this.myNavState3 = 'active';
+			      break;
+			    default:
+			      alert("There has been an error!");
+			  }
 		}
-		this.myNavState1='inactive';
-		this.myNavState2='inactive';
-		this.myNavState3='inactive';
 
-		switch (navName) {
-		    case "mod1":
-			this.myNavState1 = 'active';
-		      break;
-		    case "mod2":
-			this.myNavState2 = 'active';
-		      break;
-		    case "mod3":
-			this.myNavState3 = 'active';
-		      break;
-		    default:
-		      alert("There has been an error!");
-		  }
+
+
 	}
 
 }
